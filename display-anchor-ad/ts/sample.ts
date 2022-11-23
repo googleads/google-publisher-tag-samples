4b69ca2b608f50f09f89a1552a652c2ba3e9ae8b
Jon Imperiosi <jimper@google.com>
2022-11-23 15:52:31 -0800

    Adding TypeScript versions of display-anchor-ad, display-limited-ad, display-out-of-page-ad, and display-web-interstitial-ad samples.
    PiperOrigin-RevId: 490600546

---
+++ display-anchor-ad/ts/sample.ts
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Using @types/google-publisher-tag
+// https://www.npmjs.com/package/@types/google-publisher-tag
+
+window.googletag = window.googletag || {cmd: []};
+
+googletag.cmd.push(() => {
+  // Define an anchor ad slot at the top of the viewport.
+  // Note: a single anchor ad slot may be defined per page, at either the
+  // top or bottom of the viewport.
+  //
+  // Be aware that anchor ads are not limited to mobile devices. If you
+  // serve the same code to both mobile and non-mobile users, it is
+  // recommended to explicitly define when and where anchor ads should be
+  // shown for each environment. Here we use a basic check of viewport
+  // width to create a top anchor on mobile and a bottom anchor on
+  // non-mobile devices.
+  const anchorSlot = googletag.defineOutOfPageSlot(
+      '/6355419/Travel',
+      document.body.clientWidth <= 500 ?
+          googletag.enums.OutOfPageFormat.TOP_ANCHOR :
+          googletag.enums.OutOfPageFormat.BOTTOM_ANCHOR);
+
+  // Anchor slots return null if the page or device does not support anchors.
+  if (anchorSlot) {
+    anchorSlot.setTargeting('test', 'anchor').addService(googletag.pubads());
+
+    document.getElementById('status')!.innerText =
+        'Anchor ad is initialized. Scroll page to activate.';
+  }
+
+  // Define static ad slots.
+  const staticSlot =
+      googletag.defineSlot(
+                   '/6355419/Travel/Europe', [100, 100],
+                   'static-ad-1')!.addService(googletag.pubads());
+
+  // Enable SRA and services.
+  googletag.pubads().enableSingleRequest();
+  googletag.enableServices();
+
+  // Request and render all previously defined ad slots.
+  googletag.display(staticSlot);
+});
---
+++ display-web-interstitial-ad/ts/sample.ts
@@ -0,0 +1,48 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Using @types/google-publisher-tag
+// https://www.npmjs.com/package/@types/google-publisher-tag
+
+window.googletag = window.googletag || {cmd: []};
+
+googletag.cmd.push(() => {
+  // Define a web interstitial ad slot.
+  const interstitialSlot = googletag.defineOutOfPageSlot(
+      '/6355419/Travel/Europe/France/Paris',
+      googletag.enums.OutOfPageFormat.INTERSTITIAL);
+
+  // Slot returns null if the page or device does not support interstitials.
+  if (interstitialSlot) {
+    interstitialSlot.addService(googletag.pubads());
+
+    document.getElementById('status')!.innerText = 'Interstitial is loading...';
+
+    // Add event listener to enable navigation once the interstitial loads.
+    // If this event doesn't fire, try clearing local storage and refreshing
+    // the page.
+    googletag.pubads().addEventListener('slotOnload', (event) => {
+      if (interstitialSlot === event.slot) {
+        document.getElementById('link')!.style.display = 'block';
+        document.getElementById('status')!.innerText =
+            'Interstitial is loaded.';
+      }
+    });
+  }
+
+  // Define static ad slots.
+  const staticSlot =
+      googletag.defineSlot(
+                   '/6355419/Travel/Europe', [100, 100],
+                   'static-ad-1')!.addService(googletag.pubads());
+
+  // Enable SRA and services.
+  googletag.pubads().enableSingleRequest();
+  googletag.enableServices();
+
+  // Request and render all previously defined ad slots.
+  googletag.display(staticSlot);
+});
